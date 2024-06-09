f0e2c85c7e01b7fd2c6f944399f6094384df9a8e
Melvin Welton <nivlem65@gmail.com>
2024-06-08 22:30:39 -0500

    confirmation

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,5 @@
+var playerMoney = 10;
+
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -10,6 +12,8 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+
 
 
 // this creates a function named "fight"
@@ -28,6 +32,7 @@ function fight() {
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
 
+
         // check enemy's health
         if (enemyHealth <= 0) {
             window.alert(enemyName + " has died!");
@@ -35,11 +40,15 @@ function fight() {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
+
+
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
+
+
 
         // check player's health
         if (playerHealth <= 0) {
@@ -47,14 +56,27 @@ function fight() {
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
+
+
         // if player choses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            // subtract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
         window.alert(playerName + " has chosen to skip the fight!");
-    } else {
-        window.alert("You need to choose a valid option. Try again!");
+        console.log
+    }
+    else {
+        fight();
     }
 }
 
-    // execute function
-        fight();
-    // Add this closing curly brace
+// execute function
+fight();
+// Add this closing curly brace
